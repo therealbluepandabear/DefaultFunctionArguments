4e07bdc7008c487baf5e157d07f435904eb397d4
xTheCoderx <justaguyoney@gmail.com>
2021-10-17 02:37:55 +1300

    Implemented function 'CreateWindow()'.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-void createWindow(const char *title, int x, int y, int width, int height);
+void createWindow(std::string *title, int x, int y, int width, int height);
+
+void CreateWindow(std::string *title, int x, int y, int width, int height) {
+    std::cout << "Title : " << *title << std::endl;
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << x << std::endl;
+    std::cout << "Width: " << x << std::endl;
+    std::cout << "Height: " << x << std::endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
