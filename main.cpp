d51a3a6079a386570a7ab823c24461e7651fa888
xTheCoderx <justaguyoney@gmail.com>
2021-10-17 02:41:32 +1300

    Reformatted code.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-void createWindow(std::string *title, int x, int y, int width, int height);
+void CreateWindow(std::string *str, int x, int y, int width, int height);
 
-void CreateWindow(std::string *title, int x, int y, int width, int height) {
-    std::cout << "Title : " << *title << std::endl;
+void CreateWindow(std::string *str, int x = -1, int y = -1, int width = -1, int height = -1) {
+    std::cout << "Title : " << *str << std::endl;
     std::cout << "x: " << x << std::endl;
     std::cout << "y: " << x << std::endl;
     std::cout << "Width: " << x << std::endl;
@@ -10,5 +10,7 @@ void CreateWindow(std::string *title, int x, int y, int width, int height) {
 }
 
 int main() {
+    std::string str = "Notepad";
+    CreateWindow(&str);
     return 0;
 }
